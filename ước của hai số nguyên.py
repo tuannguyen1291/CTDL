163b8a9d30600dd884a3f8ca2e0eace064e98d55
Nguyễn Thái Tuấn <74419020+tuannguyen1291@users.noreply.github.com>
2023-03-08 22:15:38 +0700

    Delete ước của hai số nguyên.py

--- ước của hai số nguyên.py
+++
@@ -1,9 +0,0 @@
-import math
-def UCNN(a,b):
-    gcd=math.gcd(a,b)
-    print("GDC của",a,"và",b,"là: ",gcd)
-    return
-
-a=int(input("Nhấp số nguyên 1: "))
-b=int(input("Nhập số nguyên 2: "))
-UCNN(a,b)
