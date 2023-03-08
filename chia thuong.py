9b1528c8da051e09267344330aa638fa1833e1aa
Nguyễn Thái Tuấn <74419020+tuannguyen1291@users.noreply.github.com>
2023-03-08 22:22:25 +0700

    Delete chia thuong.py

--- chia thuong.py
+++
@@ -1,8 +0,0 @@
-num_users = int(input("Nhập vào số lượng người chia thưởng: "))
-bonus_amount = int(input("Nhập vào số tiền muốn chia: "))
-
-if num_users > 0:
-    bonus_per_user = bonus_amount / num_users
-    print(f"Each user will receive {bonus_per_user} bonus")
-else:
-    print("Invalid number of users")
